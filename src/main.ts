956a8c311489dbc049425ac786f3782fface0472
Marco Buschini <marco.buschini@gmail.com>
2020-07-07 14:32:06 +0200

    feat(dotenv): add dotenv support so the configuration can be read from appropriate `.env` files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { resolve } from 'path'
+import { config } from 'dotenv'
 
 async function bootstrap(): Promise<void> {
+  const envFile =
+    '.env' + (process.env.NODE_ENV !== 'prod' ? '.' + process.env.NODE_ENV : '')
+  const path = '../../' + envFile
+  console.log(`Reading configuration from ${envFile}`)
+  config({ path: resolve(__dirname, path) })
+
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
 
