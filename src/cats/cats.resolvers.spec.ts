7a4f43618246d059569f92a8de1f7c62c3d8efa0
Marco Buschini <marco.buschini@gmail.com>
2020-07-08 16:29:35 +0200

    test(resolver): expand test coverage to the resolver's subscription
    re #6

--- src/cats/cats.resolvers.spec.ts
+++ src/cats/cats.resolvers.spec.ts
@@ -68,34 +68,39 @@ describe('CatsResolver', () => {
   })
 
   describe('CR-D', () => {
-    it('create()', async () => {
-      await expect(
-        catsResolver.create({
-          name: cat1.name,
-          age: cat1.age,
-        })
-      ).resolves.toMatchObject(cat1)
-      await expect(
-        catsResolver.create({
-          name: cat2.name,
-          age: cat2.age,
-        })
-      ).resolves.toMatchObject(cat2)
+    it('create() and catCreated()', async () => {
+      const catCreated = catsResolver.catCreated()
+      const created1 = catsResolver.create({
+        name: cat1.name,
+        age: cat1.age,
+      })
+      const created2 = catsResolver.create({
+        name: cat2.name,
+        age: cat2.age,
+      })
+      expect(created1).resolves.toMatchObject(cat1)
+      expect(created2).resolves.toMatchObject(cat2)
+      expect(catCreated.next()).resolves.toMatchObject({
+        value: { catCreated: created1 },
+      })
+      expect(catCreated.next()).resolves.toMatchObject({
+        value: { catCreated: created2 },
+      })
     })
 
     it('findAll()', async () => {
       const allCats = await catsResolver.getCats()
-      await expect(allCats[0].name).toEqual(cat1.name)
-      await expect(allCats[0].age).toEqual(cat1.age)
-      await expect(allCats[1].name).toEqual(cat2.name)
-      await expect(allCats[1].age).toEqual(cat2.age)
+      expect(allCats[0].name).toEqual(cat1.name)
+      expect(allCats[0].age).toEqual(cat1.age)
+      expect(allCats[1].name).toEqual(cat2.name)
+      expect(allCats[1].age).toEqual(cat2.age)
       id = allCats[0].id
     })
 
     it('findOne()', async () => {
       const cat = await catsResolver.findOne(id)
-      await expect(cat.name).toEqual(cat1.name)
-      await expect(cat.age).toEqual(cat1.age)
+      expect(cat.name).toEqual(cat1.name)
+      expect(cat.age).toEqual(cat1.age)
     })
   })
 })
