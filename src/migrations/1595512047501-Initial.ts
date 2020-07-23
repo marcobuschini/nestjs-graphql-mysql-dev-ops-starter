7d93a301a2f5669427d1aac73913344a62c9cc80
Marco Buschini <marco.buschini@gmail.com>
2020-07-23 17:13:25 +0200

    feat(database): implement database schema migrations
    Running migrations requires that the application is built, as mitrations run only in JavaScript. The
    `ormconfig.json` file holds the database configuration to run the migrations against.

---
+++ src/migrations/1595512047501-Initial.ts
@@ -0,0 +1,73 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class Initial1595512047501 implements MigrationInterface {
+  private userTable = new Table({
+    name: 'user',
+    columns: [
+      {
+        name: 'id',
+        type: 'integer',
+        isGenerated: true,
+        isPrimary: true,
+      },
+      {
+        name: 'firstName',
+        type: 'varchar(255)',
+      },
+      {
+        name: 'lastName',
+        type: 'varchar(255)',
+      },
+      {
+        name: 'username',
+        type: 'varchar(255)',
+      },
+      {
+        name: 'password',
+        type: 'varchar(255)',
+      },
+      {
+        name: 'salt',
+        type: 'varchar(255)',
+      },
+      {
+        name: 'isActive',
+        type: 'boolean',
+      },
+      {
+        name: 'createdAt',
+        type: 'timestamp',
+      },
+      {
+        name: 'updatedAt',
+        type: 'timestamp',
+      },
+    ],
+  })
+
+  private catTable = new Table({
+    name: 'cat',
+    columns: [
+      {
+        name: 'id',
+        type: 'integer',
+        isGenerated: true,
+        isPrimary: true,
+      },
+      {
+        name: 'age',
+        type: 'integer',
+      },
+    ],
+  })
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.userTable)
+    await queryRunner.createTable(this.catTable)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.userTable)
+    await queryRunner.dropTable(this.catTable)
+  }
+}
