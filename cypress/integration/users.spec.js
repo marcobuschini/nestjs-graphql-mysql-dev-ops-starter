34531653af185d8fc0c92e1812d87449042c36c6
Marco Buschini <marco.buschini@gmail.com>
2021-10-13 17:36:22 +0200

    test(cypress): first try at testing with Cypress

---
+++ cypress/integration/users.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Backend API', () => {
+  it('verify request returns JSON', () => {
+    cy.request('http://localhost:3000/users')
+      .its('headers')
+      .its('content-type')
+      .should('include', 'application/json')
+  })
+
+  it('verify the request returns the correct status code', () => {
+    cy.request('http://localhost:3000/users')
+      .its('status')
+      .should('be.equal', 200)
+  })
+
+  it('verify the request returns 50 items', () => {
+    cy.request('http://localhost:3000/users')
+      .its('body')
+      .should('have.length', 50)
+  })
+})
