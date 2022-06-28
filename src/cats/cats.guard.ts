e5aa75112a6dc42f044b1180f47e9c91aa198d7c
Marco BUSCHINI <Marco.BUSCHINI@ext.ec.europa.eu>
2022-06-28 10:55:08 +0200

    build(npm): update depencencies
    fix #33

--- src/cats/cats.guard.ts
+++ src/cats/cats.guard.ts
@@ -1,11 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
-import { GqlExecutionContext } from '@nestjs/graphql'
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common'
+import { Observable } from 'rxjs'
 
 @Injectable()
 export class CatsGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const ctx = GqlExecutionContext.create(context)
+  public canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest()
+    return this.validateRequest(request)
+  }
+
+  protected validateRequest(
+    _request: undefined
+  ): boolean | Promise<boolean> | Observable<boolean> {
     return true
   }
 }
